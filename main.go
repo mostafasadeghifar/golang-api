dee0021c7505e3195ba7c362b37e446d1af25f5b
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-03 11:24:20 +0330

    add router to project

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	// "myjoke/route"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := mux.NewRouter();
+
+	router.HandleFunc("/tt",TestFunc)
+	log.Fatal(http.ListenAndServe(":8083",router))
+}
+
+func TestFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("hiii")
+	fmt.Println("hiii")
 }
