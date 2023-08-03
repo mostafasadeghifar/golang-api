c190e8ef08f6514743d3d21cfc07315c8dc88fc1
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-03 16:26:05 +0330

    add db auto migration

--- config/main.go
+++ config/main.go
@@ -6,9 +6,16 @@ import (
 	"fmt"
 )
 
+type Joke struct {
+	Title string `json:"title"`
+	Content string `json:"content"`
+	gorm.Model
+}
+
 func init(){
 	dsn := "root:@tcp(127.0.0.1:3306)/go-joke?charset=utf8mb4&parseTime=True&loc=Local"
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&Joke{})
 	fmt.Println(db)
 	fmt.Println(err)
 	fmt.Println("from init file")
