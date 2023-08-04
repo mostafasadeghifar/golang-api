5ce84827ccebb4d63d2554cc07623133aaed103d
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-04 11:01:33 +0330

    add route in action

--- route/JokeRoute.go
+++ route/JokeRoute.go
@@ -1,6 +1,18 @@
 package route
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+)
+
+func Routing(router *mux.Router){
+	router.HandleFunc("/samples", SampleHandler)
+}
+
+func SampleHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w,"hi babe")
+}
 
 func Sample() {
 	fmt.Println("Hello form router!")
