f2f951c5abe3de9fe1b4d99da51191c7dd98950c
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-05 11:31:02 +0430

    make struct model

--- model/Joke.go
+++ model/Joke.go
@@ -1,2 +1,20 @@
 package model
+import (
+  	"gorm.io/gorm"
+	"myjoke/config"
+)
+var (
+	db *gorm.DB
+)
+type Joke struct {
+	Title string `json:"title"`
+	Content string `json:"content"`
+	gorm.Model
+}
 
+func init(){
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Joke{})
+
+}
