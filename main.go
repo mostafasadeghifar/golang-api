13753aefa71a931f47cf64c872f0f9836c160549
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-05 11:29:59 +0430

    refactor config connection

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"myjoke/config"
 	"myjoke/route"
 	"net/http"
 
@@ -13,7 +12,5 @@ func main() {
 	router := mux.NewRouter()
 	// route.Routing(router)
 	route.JokeRout(router)
-	config.Connect();
-
 	log.Fatal(http.ListenAndServe(":8083",router))
 }
