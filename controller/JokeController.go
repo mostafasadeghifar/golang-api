1aa3aec77caa4cbb6e894d8a1e09753733b0e3e3
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-08-05 17:37:40 +0430

    add create to joke controller

--- controller/JokeController.go
+++ controller/JokeController.go
@@ -1,10 +1,31 @@
 package controller
 
 import (
-	"net/http"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+	"myjoke/model"
 )
 
 func Index(http.ResponseWriter, *http.Request) {
 	fmt.Println("from controller")
+}
+
+func Create(w http.ResponseWriter,req *http.Request) {
+	CreateJoke := &model.Joke{}
+	ParseBody(req, CreateJoke)
+	fmt.Println(&CreateJoke)
+	b := CreateJoke.CreateJoke()
+	res, _ := json.Marshal(b)
+	w.Write(res)
+
+}
+
+func ParseBody(r *http.Request,X interface{}){
+	if body, err := ioutil.ReadAll(r.Body); err==nil{
+		if err:= json.Unmarshal([]byte(body),X); err != nil {
+			return 
+		}
+	}
 }
